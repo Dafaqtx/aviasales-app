e628951bc34616258e657000a31ff39b98c6493b
Alexander Kuchuk <dafaqtx@gmail.com>
2019-11-04 14:01:21 +0300

    added sidebar and main content layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../Header'
+import Sidebar from '../Sidebar'
+import Content from '../Content'
 import './styles.scss'
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Sidebar />
+        <Content />
     </div>
   );
 }
