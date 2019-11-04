09452176e5d1dd658e51d9276ba05302dd53b9e8
Alexander Kuchuk <dafaqtx@gmail.com>
2019-11-04 18:34:56 +0300

    added Segment component

---
+++ src/components/Segment/Segment.js
@@ -0,0 +1,49 @@
+import React from "react";
+import moment from "moment";
+
+import {
+  formattedMinutes,
+  formattedStops,
+  fromattedTrans
+} from "../../formatters";
+
+import "./style.scss";
+
+const Segment = ({ segments }) => {
+  return (
+    <div className="Segment">
+      {segments.map((segment, id) => (
+        <div className="segment__row" key={id}>
+          <div className="segment__col segment__path">
+            <span className="segment__text">{`${segment.origin} - ${segment.destination}`}</span>
+            <div className="segment__value">
+              {`${moment(segment.date).format("HH:mm")} – ${moment(segment.date)
+                .add(segment.duration, "minutes")
+                .format("HH:mm")}`}
+            </div>
+          </div>
+          <div className="segment__col segment__time">
+            <span className="segment__text">В пути</span>
+            <div className="segment__value">
+              {`${formattedMinutes(segment.duration)}`}
+            </div>
+          </div>
+          <div className="segment__col segment__stops">
+            <span className="segment__text">
+              {fromattedTrans(segment.stops.length)}
+            </span>
+            <div className="segment__value">
+              {segment.stops.map((stop, id) => (
+                <span key={id}>
+                  {formattedStops(stop, id + 1 === segment.stops.length)}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Segment;
--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -1,64 +1,16 @@
 import React from "react";
-import moment from 'moment';
 
-import TicketHeader from '../TicketHeader'
-import './style.scss';
+import TicketHeader from "../TicketHeader";
+import Segment from "../Segment";
+import "./style.scss";
 
-const Ticket = ({ticket}) => {
-
-  const formattedMinutes = minutes => {
-    const formattedHours = Math.floor((minutes / 60));
-    const formattedMinutes = Math.round(((minutes / 60) - formattedHours) * 60);
-
-    return `${formattedHours}ч ${formattedMinutes}м`;
-  }
-
-  const formattedStops = (stop, last) => {
-    if (!last) {
-        return `${stop}, `;
-    } else {
-        return stop;
-    }
-  }
-
-  const fromattedTrans = (count) => {
-    if (count === 0) {
-        return 'Без пересадок';
-    }
-    
-    return count === 1 ? '1 пересадка' : `${count} пересадки`;
-  }
-
-
+const Ticket = ({ ticket }) => {
   return (
-        <div className="Ticket">
-            <TicketHeader price={ticket.price} carrier={ticket.carrier} />
-            <div className="Ticket__segments segment">
-                {ticket.segments.map((segment, id) => (
-                    <div className="segment__row" key={id}>
-                        <div className="segment__col segment__path">
-                            <span className="segment__text">{`${segment.origin} - ${segment.destination}`}</span>
-                            <div className="segment__value">
-                                {`${moment(segment.date).format('HH:mm')} – ${moment(segment.date).add(segment.duration, 'minutes').format('HH:mm')}`}
-                            </div>
-                        </div>
-                        <div className="segment__col segment__time">
-                            <span className="segment__text">В пути</span>
-                            <div className="segment__value">
-                                {`${formattedMinutes(segment.duration)}`}
-                            </div>
-                        </div>
-                        <div className="segment__col segment__stops">
-                            <span className="segment__text">{fromattedTrans(segment.stops.length)}</span>
-                            <div className="segment__value">
-                                {segment.stops.map((stop, id) => (<span key={id}>{formattedStops(stop, id + 1 === segment.stops.length)}</span>))}
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+    <div className="Ticket">
+      <TicketHeader price={ticket.price} carrier={ticket.carrier} />
+      <Segment segments={ticket.segments} />
+    </div>
+  );
 };
 
 export default Ticket;
