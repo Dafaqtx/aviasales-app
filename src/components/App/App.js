7f0c507629d8b33db97571d29b09b2a9e47ff51e
Alexander Kuchuk <dafaqtx@gmail.com>
2019-11-04 14:46:55 +0300

    added getTickets request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,16 +6,28 @@ import Content from '../Content'
 import './styles.scss'
 
 function App() {
-  const searchIdUrl = 'https://front-test.beta.aviasales.ru/search';
+  const url = 'https://front-test.beta.aviasales.ru';
   const [searchId, setSearchId] = useState([])
+  const [tickets, setTickets] = useState([])
 
   useEffect(() => {
     const getSearchId = async () => {
-      const result = await axios(searchIdUrl);
+      const result = await axios(`${url}/search`);
       setSearchId(result.data.searchId);
     };
     getSearchId();
   }, []);
+
+
+  useEffect(() => {
+    const getTickets = async () => {
+      const result = await axios(
+        `${url}/tickets?searchId=${searchId}`,
+      );
+      setTickets(result.data.tickets);
+    };
+    getTickets();
+  }, [searchId]);
 
   return (
     <div className="App">
