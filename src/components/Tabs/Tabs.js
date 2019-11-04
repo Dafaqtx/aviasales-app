acf041b4b37f76de25fc250b38aaddd03797862c
Alexander Kuchuk <dafaqtx@gmail.com>
2019-11-04 15:50:30 +0300

    added functionality for tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import "./style.scss";
 
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState("economical");
   return (
     <div className="Tabs">
-        <button type="button" className="Tabs__button Tabs__button--active">Самый дешевый</button>
-        <button type="button" className="Tabs__button">Самый быстрый</button>
+      <button
+        type="button"
+        className={classNames("Tabs__button", {
+          "Tabs__button--active": activeTab === "economical"
+        })}
+        onClick={() => {
+          setActiveTab("economical");
+        }}
+      >
+        Самый дешевый
+      </button>
+      <button
+        type="button"
+        className={classNames("Tabs__button", {
+          "Tabs__button--active": activeTab === "fast"
+        })}
+        onClick={() => {
+          setActiveTab("fast");
+        }}
+      >
+        Самый быстрый
+      </button>
     </div>
   );
 };
