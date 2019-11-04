a2f2592555fa7a3fbd15a0f228969fb479ae62a3
Alexander Kuchuk <dafaqtx@gmail.com>
2019-11-04 14:30:51 +0300

    added axios and serachId request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Header from '../Header'
 import Sidebar from '../Sidebar'
 import Content from '../Content'
 import './styles.scss'
 
 function App() {
+  const searchIdUrl = 'https://front-test.beta.aviasales.ru/search';
+  const [searchId, setSearchId] = useState([])
+
+  useEffect(() => {
+    const getSearchId = async () => {
+      const result = await axios(searchIdUrl);
+      setSearchId(result.data.searchId);
+    };
+    getSearchId();
+  }, []);
+
   return (
     <div className="App">
         <Header />
