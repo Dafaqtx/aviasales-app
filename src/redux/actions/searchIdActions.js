6e4204127428e981b6b266bcb673b7c016a0fed9
Alexander Kuchuk <dafaqtx@gmail.com>
2019-11-05 23:20:45 +0300

    getSearchId api in action

--- src/redux/actions/searchIdActions.js
+++ src/redux/actions/searchIdActions.js
@@ -1,14 +1,24 @@
-import {
-  action,
-  createRequestTypes,
-  REQUEST,
-  FAILURE,
-  SUCCESS
-} from "../reduxHelpers";
+import { createRequestTypes } from "../reduxHelpers";
+
+import { getSearchId } from "../api";
+
+export const fetchSearchId = () => {
+  return dispatch => {
+    dispatch({ type: "GET_SEARCH_ID_REQUEST" });
+    getSearchId()
+      .then(res => {
+        const searchId = res.data.searchId;
+        dispatch({ type: "GET_SEARCH_ID_SUCCESS", data: searchId });
+      })
+      .catch(error => {
+        dispatch({ type: "GET_SEARCH_ID_FAILURE", error: error.message });
+      });
+  };
+};
 
 export const GET_SEARCH_ID = createRequestTypes("GET_SEARCH_ID");
-export const getSearchId = {
-  request: () => action(GET_SEARCH_ID[REQUEST]),
-  success: data => action(GET_SEARCH_ID[SUCCESS], { data }),
-  failure: error => action(GET_SEARCH_ID[FAILURE], { error })
-};
+// export const getSearchId = {
+//   request: () => action(GET_SEARCH_ID[REQUEST]),
+//   success: data => action(GET_SEARCH_ID[SUCCESS], { data }),
+//   failure: error => action(GET_SEARCH_ID[FAILURE], { error })
+// };
