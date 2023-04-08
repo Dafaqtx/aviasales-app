048876136ddee4ec431f9f1823e13cb11c5c7175
Alexander Kuchuk <Dafaqtx@gmail.com>
2023-04-08 13:26:45 +0300

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,34 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { legacy_createStore as createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
 
 import reducer from "./reducers";
 
-export default function() {
-  const middlewares = [thunk];
 
-  let composeEnhancers = compose;
+const middlewares = [thunk];
 
-  // connect browser DevTools and Logger on dev mode
-  if (process.env.NODE_ENV === "development") {
-    const logger = createLogger({
-      collapsed: true,
-      duration: true
-    });
-    middlewares.push(logger);
+let composeEnhancers = compose;
 
-    const composeWithDevToolsExtension =
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; // eslint-disable-line
-    if (typeof composeWithDevToolsExtension === "function") {
-      composeEnhancers = composeWithDevToolsExtension;
-    }
+// connect browser DevTools and Logger on dev mode
+if (process.env.NODE_ENV === "development") {
+  const logger = createLogger({
+    collapsed: true,
+    duration: true
+  });
+  middlewares.push(logger);
+
+  const composeWithDevToolsExtension =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; // eslint-disable-line
+  if (typeof composeWithDevToolsExtension === "function") {
+    composeEnhancers = composeWithDevToolsExtension;
   }
-
-  const store = createStore(
-    reducer,
-    composeEnhancers(applyMiddleware(...middlewares))
-  );
-  return store;
 }
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+
+export default store
+
